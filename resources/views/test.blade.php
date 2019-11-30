96e7351366a44a06bdaaea7ee0658ca89928bd43
shehryar <shehryar.haider575@gmail.com>
2019-11-29 23:44:48 -0800

    Blade Template Editing

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('content')
     @if(session('message'))
         <h4>{{session('message')}}</h4>
     @endif
@@ -40,5 +33,4 @@
             
         </tbody>
     </table>
-</body>
-</html>
+@endsection
